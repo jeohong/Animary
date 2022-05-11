d858154589c824bc1b38bd55da5fc7edc9a01a0a
jeohong <jeohong59@gmail.com>
2022-05-11 23:16:36 +0900

    TabBarController 적용

---
+++ Animary/Controller/DiaryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryViewController.swift
+//  Animary
+//
+//  Created by Hong jeongmin on 2022/05/11.
+//
+
+import UIKit
+
+class DiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
